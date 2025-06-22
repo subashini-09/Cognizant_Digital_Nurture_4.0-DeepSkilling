64c2327eeb5eb2943784585b0947552621aa2356
SUBASHINI MATHI.G <141746869+subashini-09@users.noreply.github.com>
2025-06-22 16:23:09 +0530

    Create PushNotification.java

---
+++ Week1_Design principles & Patterns/Exercise 2: Implementing the Factory Method Pattern/Code/PushNotification.java
@@ -0,0 +1,7 @@
+package com.example.factory;
+
+public class PushNotification implements Notification {
+    public void notifyUser() {
+        System.out.println("Sending a Push Notification");
+    }
+}
