dec9b704e460cdcfb4e93ff7d3687e0e54a5cf64
SUBASHINI MATHI.G <141746869+subashini-09@users.noreply.github.com>
2025-06-29 17:29:40 +0530

    Create CalculatorTest.java

---
+++ Week2_1. JUnit_Basic Testing Exercises/Exercise 1: Setting Up JUnit/Code/CalculatorTest.java
@@ -0,0 +1,14 @@
+
+package com.example.tdd;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+
+    @Test
+    void testAddition() {
+        Calculator calculator = new Calculator();
+        assertEquals(5, calculator.add(2, 3));
+    }
+}
