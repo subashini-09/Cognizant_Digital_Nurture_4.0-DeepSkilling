a6eb67dc3cc03de3ea58d96910cadfd94deb96f9
SUBASHINI MATHI.G <141746869+subashini-09@users.noreply.github.com>
2025-07-05 19:31:06 +0530

    Create MainApp.java

---
+++ Week3_Spring Core and Maven/Spring Core_Maven/Exercise 2: Implementing Dependency Injection/Code/MainApp.java
@@ -0,0 +1,12 @@
+package com.example.springdi;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class MainApp {
+    public static void main(String[] args) {
+        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+        Car car = context.getBean(Car.class);
+        car.showEngineType();  // Outputs: Car is running with: Petrol Engine
+    }
+}
