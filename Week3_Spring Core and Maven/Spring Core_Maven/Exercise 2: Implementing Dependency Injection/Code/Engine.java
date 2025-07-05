fe3834e4a16f1f7cc8a8a87bda3f524677ea39ee
SUBASHINI MATHI.G <141746869+subashini-09@users.noreply.github.com>
2025-07-05 19:29:54 +0530

    Create Engine.java

---
+++ Week3_Spring Core and Maven/Spring Core_Maven/Exercise 2: Implementing Dependency Injection/Code/Engine.java
@@ -0,0 +1,13 @@
+package com.example.springdi;
+
+public class Engine {
+    public String type;
+
+    public Engine() {
+        this.type = "Petrol Engine";
+    }
+
+    public String getType() {
+        return type;
+    }
+}
