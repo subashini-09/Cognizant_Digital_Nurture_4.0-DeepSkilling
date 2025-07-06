815128c841be995cc6fedfb23ac5c8e181ece38f
SUBASHINI MATHI.G <141746869+subashini-09@users.noreply.github.com>
2025-07-06 13:39:03 +0530

    Create StudentDemoApplication.java

---
+++ Week3_Spring Data JPA with Spring Boot, Hibernate/1. spring-data-jpa-handson/Spring Data JPA - Quick Example/Code/StudentDemoApplication.java
@@ -0,0 +1,11 @@
+package com.example.studentdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentDemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(StudentDemoApplication.class, args);
+    }
+}
