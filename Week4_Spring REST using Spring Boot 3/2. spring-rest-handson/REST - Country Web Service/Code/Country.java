81de2acd3dbde7890bc50f7eee54b97114b7f210
SUBASHINI MATHI.G <141746869+subashini-09@users.noreply.github.com>
2025-07-13 18:42:40 +0530

    Create Country.java

---
+++ Week4_Spring REST using Spring Boot 3/2. spring-rest-handson/REST - Country Web Service/Code/Country.java
@@ -0,0 +1,29 @@
+package com.example.countryrest.model;
+
+public class Country {
+    private String code;
+    private String name;
+
+    // Default constructor
+    public Country() {}
+
+    // Constructor with parameters
+    public Country(String code, String name) {
+        this.code = code;
+        this.name = name;
+    }
+
+    // Getters and Setters
+    public String getCode() {
+        return code;
+    }
+    public void setCode(String code) {
+        this.code = code;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+}
