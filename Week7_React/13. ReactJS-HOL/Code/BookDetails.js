89c2219913b05b392fa44cbbeb53d8642510f150
SUBASHINI MATHI.G <141746869+subashini-09@users.noreply.github.com>
2025-08-03 23:07:20 +0530

    Create BookDetails.js

---
+++ Week7_React/13. ReactJS-HOL/Code/BookDetails.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const BookDetails = () => (
+  <div>
+    <h2>📖 Book Details</h2>
+    <p>Title: The Pragmatic Programmer</p>
+    <p>Author: Andrew Hunt & David Thomas</p>
+  </div>
+);
+
+export default BookDetails;
