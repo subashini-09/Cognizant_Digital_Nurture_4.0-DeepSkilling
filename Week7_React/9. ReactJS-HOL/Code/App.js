451149c68cdf86279b51a7e6036e8e25b272614a
SUBASHINI MATHI.G <141746869+subashini-09@users.noreply.github.com>
2025-08-03 20:31:50 +0530

    Create App.js

---
+++ Week7_React/9. ReactJS-HOL/Code/App.js
@@ -0,0 +1,17 @@
+
+import React from 'react';
+import ListofPlayers from './ListofPlayers';
+import IndianPlayers from './IndianPlayers';
+
+function App() {
+  const flag = true; // Change to false to see IndianPlayers
+
+  return (
+    <div className="App">
+      <h1>🏏 Cricket App</h1>
+      {flag ? <ListofPlayers /> : <IndianPlayers />}
+    </div>
+  );
+}
+
+export default App;
