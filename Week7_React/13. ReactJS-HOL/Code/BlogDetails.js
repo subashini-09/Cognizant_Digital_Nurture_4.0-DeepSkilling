09659009bfdca1d36576c7a2e1f608ed8f320a87
SUBASHINI MATHI.G <141746869+subashini-09@users.noreply.github.com>
2025-08-03 23:07:02 +0530

    Create BlogDetails.js

---
+++ Week7_React/13. ReactJS-HOL/Code/BlogDetails.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const BlogDetails = () => (
+  <div>
+    <h2>✍️ Blog Details</h2>
+    <p>Title: Learning React in 2025</p>
+    <p>Author: Subashini Mathi</p>
+  </div>
+);
+
+export default BlogDetails;
