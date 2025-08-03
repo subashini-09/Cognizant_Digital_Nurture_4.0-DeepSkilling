4e6c9ef9cc8d1970d6f0973c4834ea5a796dcbc2
SUBASHINI MATHI.G <141746869+subashini-09@users.noreply.github.com>
2025-08-03 23:04:49 +0530

    Create CourseDetails.js

---
+++ Week7_React/13. ReactJS-HOL/Code/CourseDetails.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CourseDetails = () => (
+  <div>
+    <h2>🎓 Course Details</h2>
+    <p>Course: React Full Stack</p>
+    <p>Duration: 6 Months</p>
+  </div>
+);
+
+export default CourseDetails;
