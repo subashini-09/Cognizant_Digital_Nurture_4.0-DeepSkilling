c28131376bef406bf040777c8e7539ffa4930ab0
SUBASHINI MATHI.G <141746869+subashini-09@users.noreply.github.com>
2025-08-03 20:49:52 +0530

    Create App.js

---
+++ Week7_React/10. ReactJS-HOL/Code/App.js
@@ -0,0 +1,20 @@
+const officeList = [
+  {
+    name: 'Global Tech Park',
+    rent: 55000,
+    address: 'Whitefield, Bangalore',
+    image: '/office1.jpg'  
+  },
+  {
+    name: 'Manyata Embassy',
+    rent: 62000,
+    address: 'Hebbal, Bangalore',
+    image: '/office2.jpg'
+  },
+  {
+    name: 'RMZ Infinity',
+    rent: 45000,
+    address: 'Old Madras Road, Bangalore',
+    image: '/office3.jpg'
+  }
+];
